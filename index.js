3480e59399732bd90283c34bc67aaf27f169e675
Daniel <df246479@gmail.com>
2024-02-21 21:40:27 -0300

    finalizado

---
+++ index.js
@@ -0,0 +1,19 @@
+import  express  from 'express';
+import process from 'process';
+import path from 'path';
+
+const host = 'localhost';
+const porta = 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(process.cwd(), 'publico', 'DanielT.html'));
+});
+
+app.use(express.static(path.join(process.cwd(), 'publico')));
+
+
+app.listen(porta, host, ()=>{
+    console.log(`Servidor escutando em http://${host}:${porta}`)
+})
